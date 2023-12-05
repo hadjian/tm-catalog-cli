7b8b9eaf039cb59a5e0866ecf35f399c1da2d14e
Pedram Hadjian <pedram.hadjian@gmail.com>
2023-12-05 23:54:43 +0100

    fix

--- docker/download-cli.sh
+++ docker/download-cli.sh
@@ -2,6 +2,4 @@
 
 URL=$(curl -s https://api.github.com/repos/web-of-things-open-source/tm-catalog-cli/releases/latest | jq -r '.assets | .[] | select(.name = "tm-catalog-cli-linux_amd64") | .browser_download_url')
 
-
-| jq -r '.assets | .[] | select(.name = "tm-catalog-cli-linux_amd64") | .browser_download_url'
 curl -OL $URL
