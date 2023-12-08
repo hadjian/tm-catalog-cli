295fbdba6609dce0f5675d6dd86a76a89c222e45
Pedram Hadjian <pedram.hadjian@gmail.com>
2023-12-08 14:46:48 +0100

    chore(docker): downloading cli release in Dockerfile for easier usage

--- docker/download-cli.sh
+++ docker/download-cli.sh
@@ -3,3 +3,4 @@
 URL=$(curl -s https://api.github.com/repos/web-of-things-open-source/tm-catalog-cli/releases/latest | jq -r '.assets | .[] | select(.name = "tm-catalog-cli-linux_amd64") | .browser_download_url')
 
 curl -OL $URL
+mv ./tm-catalog-cli-linux_amd64 ./tm-catalog-cli
