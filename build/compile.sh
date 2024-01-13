66e8f43b781b61df59e1752e1e41ab0a2663a77d
Pedram Hadjian <pedram.hadjian@gmail.com>
2024-01-13 23:31:08 +0100

    filename not picked up

--- build/compile.sh
+++ build/compile.sh
@@ -5,7 +5,7 @@
 # FILENAME_PREFIX: "tm-catalog-cli" per default and normally set to the repo name in a pipeline
 # GOOS_GARCH_TARGETS: list of of os/arch compile targets with defaults
    
-file_name_prefix=${FILENAME_PREFIX:-"tm-catalog-cli"}
+filename_prefix=${FILENAME_PREFIX:-"tm-catalog-cli"}
 targets=${GOOS_GARCH_TARGETS:-"linux,amd64;darwin,amd64;darwin,arm64;windows,amd64;windows,arm64"}
 
 IFS=';' read -ra os_arch_array <<< $targets 
@@ -14,7 +14,7 @@ echo "building targets: $targets"
 for os_arch in "${os_arch_array[@]}"; do
   export GOOS="${os_arch%%,*}"
   export GARCH="${os_arch#*,}"
-  filename="${FILE_NAME_PREFIX}-${GOOS}-${GARCH}"
+  filename="${filename_prefix}-${GOOS}-${GARCH}"
   echo "compiling $filename"
   CGO_ENABLED=0 go build -o $filename
 done
